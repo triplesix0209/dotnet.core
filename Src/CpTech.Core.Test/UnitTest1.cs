1a41d4614e41c1a659cd140b375d2b176bbf6322
Luc THQ <luc.ta@cptech.vn>
2021-12-28 14:46:06 +0700

    init

---
+++ Src/CpTech.Core.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CpTech.Core.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
