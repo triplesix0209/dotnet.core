5229f34246cfaa4204297e1560eb41a6fe1a211e
Luc THQ <luc.ta@cptech.vn>
2021-12-30 18:13:54 +0700

    up

---
+++ Src/TripleSix.Core.Test/Extensions/DateTimeExtensionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TripleSix.Core.Extensions;
+
+namespace TripleSix.Core.Test.Extensions
+{
+    [TestClass]
+    public class DateTimeExtensionTest
+    {
+        [TestMethod]
+        public void ToEpochTimestamp()
+        {
+            var datetime = new DateTime(1993, 9, 2, 4, 0, 0);
+            var timestamp = 746942400000;
+
+            Assert.AreEqual(datetime.ToEpochTimestamp(), timestamp);
+        }
+    }
+}
--- Src/TripleSix.Core/Extensions/StringExtension.cs
+++ Src/TripleSix.Core/Extensions/StringExtension.cs
@@ -24,6 +24,16 @@ namespace TripleSix.Core.Extensions
             "ÝỲỴỶỸ",
         };
 
+        public static bool IsNullOrWhiteSpace(this string text)
+        {
+            return string.IsNullOrWhiteSpace(text);
+        }
+
+        public static bool IsNotNullOrWhiteSpace(this string text)
+        {
+            return !string.IsNullOrWhiteSpace(text);
+        }
+
         public static string RemoveVietnameseSign(this string text)
         {
             for (int i = 1; i < VietnameseSigns.Length; i++)
@@ -33,6 +43,14 @@ namespace TripleSix.Core.Extensions
             }
 
             return text;
+        }
+
+        public static string ToCamelCase(this string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return text;
+            text = string.Join(string.Empty, text.Split(" ").Where(x => x.Length > 0));
+            text = text.Replace(" ", "_");
+            return Regex.Replace(text, @"([a-z0-9])([A-Z])", "$1_$2").ToLower();
         }
 
         public static string ToSnakeCase(this string text)
@@ -49,23 +67,6 @@ namespace TripleSix.Core.Extensions
             text = string.Join(" ", text.Split(" ").Where(x => x.Length > 0));
             text = text.Replace(" ", "-");
             return Regex.Replace(text, @"([a-z0-9])([A-Z])", "$1-$2").ToLower();
-        }
-
-        public static string ToCamelCase(this string text)
-        {
-            return string.IsNullOrWhiteSpace(text) || text.Length < 2
-                ? text
-                : char.ToLowerInvariant(text[0]) + text.Substring(1);
-        }
-
-        public static bool IsNullOrWhiteSpace(this string text)
-        {
-            return string.IsNullOrWhiteSpace(text);
-        }
-
-        public static bool IsNotNullOrWhiteSpace(this string text)
-        {
-            return !string.IsNullOrWhiteSpace(text);
         }
     }
 }
