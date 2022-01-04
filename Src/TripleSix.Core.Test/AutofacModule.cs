f75ee94bb2c197ff446dd84ea1070124a85a175d
Luc THQ <luc.ta@cptech.vn>
2022-01-04 11:25:23 +0700

    up

---
+++ Src/TripleSix.Core.Test/AutofacModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using TripleSix.Core.ModuleAutofac;
+
+namespace TripleSix.Core.Test
+{
+    public class AutofacModule : BaseModule
+    {
+        public AutofacModule(IConfiguration configuration)
+            : base(configuration)
+        {
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterEventPublisher();
+        }
+    }
+}
