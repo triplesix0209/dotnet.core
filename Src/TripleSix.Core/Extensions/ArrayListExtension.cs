d579ed84b75926fe7a5bbb80b47d61b8cf18039b
Luc THQ <luc.ta@cptech.vn>
2022-02-03 10:52:30 +0700

    Contains Any

--- Src/TripleSix.Core/Extensions/ArrayListExtension.cs
+++ Src/TripleSix.Core/Extensions/ArrayListExtension.cs
@@ -5,6 +5,39 @@ namespace TripleSix.Core.Extensions
 {
     public static class ArrayListExtension
     {
+        public static bool ContainAny<T>(this T[] array, params T[] values)
+        {
+            foreach (var value in values)
+            {
+                if (array.Contains(value))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool ContainAny<T>(this IList<T> list, params T[] values)
+        {
+            foreach (var value in values)
+            {
+                if (list.Contains(value))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool ContainAny<T>(this IEnumerable<T> enumerable, params T[] values)
+        {
+            foreach (var value in values)
+            {
+                if (enumerable.Contains(value))
+                    return true;
+            }
+
+            return false;
+        }
+
         public static bool IsNullOrEmpty<T>(this T[] array)
         {
             return array == null || array.Length == 0;
