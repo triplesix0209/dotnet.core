7e355d450b9acdd2fd736a6a4e721b234625053e
Luc THQ <luc.ta@cptech.vn>
2022-02-25 20:54:26 +0700

    up

--- Src/Sample.WebApi/Controllers/Admins/Auto/MetadataController.cs
+++ Src/Sample.WebApi/Controllers/Admins/Auto/MetadataController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Sample.Common.Enum;
 using TripleSix.Core.AutoAdmin;
+using TripleSix.Core.WebApi.Filters;
 
 namespace Sample.WebApi.Controllers.Admins.Auto
 {
     [Route("Admin/_[controller]")]
     [Authorize(AuthenticationSchemes = "account-token")]
+    [AccessLevelRequirement(MinimumAccountLevel = (int)AccountLevels.Admin)]
     public class MetadataController : BaseAdminMetadataController
     {
     }
