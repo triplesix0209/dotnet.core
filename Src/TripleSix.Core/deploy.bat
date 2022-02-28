e707c3b33e0cc1fa51b67268f64c98c25da95c9e
Luc THQ <luc.ta@cptech.vn>
2022-02-28 15:30:31 +0700

    init

--- Src/TripleSix.Core/deploy.bat
+++ Src/TripleSix.Core/deploy.bat
@@ -1,1 +1,1 @@
-dotnet nuget push .\dist\TripleSix.Core.5.0.18.nupkg --source https://api.nuget.org/v3/index.json --api-key oy2ilyorryhglnpfpkwasw3fpc3znsrelbmg4svzb5zzw4
+dotnet nuget push .\dist\TripleSix.Core.5.0.19.nupkg --source https://api.nuget.org/v3/index.json --api-key oy2ilyorryhglnpfpkwasw3fpc3znsrelbmg4svzb5zzw4
