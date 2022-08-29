dfa1ac343f78c88e253965aad87c821988f5b750
Luc THQ <luc.ta@cptech.vn>
2022-08-29 17:15:35 +0700

    up

--- Src/WebApi/Controllers/Admins/SettingController.cs
+++ Src/WebApi/Controllers/Admins/SettingController.cs
@@ -1,7 +1,10 @@
 ﻿namespace Sample.WebApi.Controllers.Admins
 {
     [SwaggerTag("thiết lập")]
-    [AdminController(ModelType = typeof(SettingAdminDto), EnableCreate = false, EnableDelete = false)]
+    [AdminController(
+        ModelType = typeof(SettingAdminDto),
+        EnableCreate = false,
+        EnableDelete = false)]
     public class SettingController : AdminController
     {
     }
