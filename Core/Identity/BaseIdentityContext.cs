52dd628f296c8adf6136c5b7b36398ae3709ee21
lucthq-giganteast <lucthq@giganteast.com>
2023-10-20 14:50:42 +0700

    up

--- Core/Identity/BaseIdentityContext.cs
+++ Core/Identity/BaseIdentityContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Http;
 
 namespace TripleSix.Core.Identity
 {
@@ -10,6 +11,31 @@ namespace TripleSix.Core.Identity
         /// <param name="httpContext"><see cref="HttpContext"/>.</param>
         protected BaseIdentityContext(HttpContext? httpContext)
         {
+            if (httpContext == null)
+            {
+                IsVaild = false;
+                return;
+            }
+
+            try
+            {
+                var accessToken = httpContext.Request.Headers.Authorization.First();
+                var jwtToken = new JwtSecurityTokenHandler().ReadJwtToken(accessToken);
+                LoadDataFromToken(jwtToken);
+                IsVaild = true;
+            }
+            catch
+            {
+                IsVaild = false;
+            }
         }
+
+        // Phiên xác thực hợp lệ
+        public bool IsVaild { get; }
+
+        // Id tài khoản
+        public Guid Id { get; set; }
+
+        protected abstract void LoadDataFromToken(JwtSecurityToken token);
     }
 }
--- Core/Identity/Interfaces/IIdentityContext.cs
+++ Core/Identity/Interfaces/IIdentityContext.cs
@@ -2,5 +2,14 @@
 {
     public interface IIdentityContext
     {
+        /// <summary>
+        /// Đã xác thực thành công.
+        /// </summary>
+        public bool IsVaild { get; }
+
+        /// <summary>
+        /// Id người thao tác.
+        /// </summary>
+        public Guid Id { get; set; }
     }
 }
