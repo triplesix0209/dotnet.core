313c0a946350cba62f594b4f1712a85b73405e51
lucthq-giganteast <lucthq@giganteast.com>
2023-10-23 19:25:11 +0700

    up

--- Sample/WebApi/Controllers/Apps/AccountController.cs
+++ Sample/WebApi/Controllers/Apps/AccountController.cs
@@ -4,13 +4,5 @@
     public class AccountController : AppController
     {
         public IAccountService AccountService { get; set; }
-
-        [HttpGet]
-        [SwaggerOperation("Lấy danh sách tài khoản")]
-        public async Task<PagingResult<AccountDataDto>> GetAll(PagingInputDto input)
-        {
-            var result = await AccountService.GetPageActivate(input.Page, input.Size);
-            return PagingResult(result);
-        }
     }
 }
