72e14d0c59f9ab3251bb65cfec04eea9e02d6360
Luc THQ <triplesix0209@gmail.com>
2023-11-15 00:56:38 +0700

    fix not found exception code

--- Sample/WebApi/Controllers/Admins/SiteController.cs
+++ Sample/WebApi/Controllers/Admins/SiteController.cs
@@ -1,9 +1,16 @@
-﻿namespace Sample.WebApi.Controllers.Admins
+﻿using TripleSix.Core.Exceptions;
+
+namespace Sample.WebApi.Controllers.Admins
 {
     [SwaggerTag("chi nhánh")]
     [ReadEndpoint<SiteController, Site, SiteDataAdminDto, SiteFilterAdminDto>]
     [CreateEndpoint<SiteController, Site, SiteCreateAdminDto>]
     public class SiteController : AdminController
     {
+        [HttpGet("Test")]
+        public async Task<SuccessResult> Test()
+        {
+            throw new NotFoundException<Site>();
+        }
     }
 }
