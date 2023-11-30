d678b9c8377c25b4bcfebb5391bbaa3bf47d089a
Luc THQ <triplesix0209@gmail.com>
2023-11-30 16:03:15 +0700

    up

---
+++ Core/Appsettings/HangfireWorkerAppsetting.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using TripleSix.Core.Helpers;
+
+namespace TripleSix.Core.Appsettings
+{
+    /// <summary>
+    /// Cấu hình hangfire worker.
+    /// </summary>
+    public class HangfireWorkerAppsetting : BaseAppsetting
+    {
+        /// <summary>
+        /// Cấu hình elasticsearch.
+        /// </summary>
+        /// <param name="configuration"><see cref="IConfiguration"/>.</param>
+        public HangfireWorkerAppsetting(IConfiguration configuration)
+            : base(configuration, "HangfireWorker")
+        {
+            if (Enable == false) return;
+
+            if (ConnectionString.IsNullOrEmpty()) throw new ArgumentNullException(nameof(ConnectionString));
+            if (Queues.IsNullOrEmpty()) throw new ArgumentNullException(nameof(Queues));
+        }
+
+        /// <summary>
+        /// Bật/tắt hangfire worker (mặc định là true).
+        /// </summary>
+        public bool Enable { get; set; } = true;
+
+        /// <summary>
+        /// Database connection string.
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        /// Danh sách queue listen.
+        /// </summary>
+        public string[]? Queues { get; set; }
+    }
+}
