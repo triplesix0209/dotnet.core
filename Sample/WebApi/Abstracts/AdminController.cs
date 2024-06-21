4052c62be3fe1824ffa1918e65b84cee11582ab8
Luc THQ <triplesix0209@gmail.com>
2024-06-21 22:51:58 +0700

    up

--- Sample/WebApi/Abstracts/AdminController.cs
+++ Sample/WebApi/Abstracts/AdminController.cs
@@ -3,7 +3,6 @@
     [Route("Admin/[controller]")]
     [Authorize]
     [RequireScope("admin")]
-    [RequireIssuer("SAMPLE")]
     [SwaggerTagGroup("Admin", 2)]
     public abstract class AdminController : BaseController
     {
