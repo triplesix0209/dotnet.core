af21bf04e8467ebefb78ee1a95ad5b49ab13195b
Luc THQ <triplesix0209@gmail.com>
2024-06-21 22:39:23 +0700

    up

--- Core/Mappers/Attributes/MapFromEntityAttribute.cs
+++ Core/Mappers/Attributes/MapFromEntityAttribute.cs
@@ -10,6 +10,19 @@ namespace TripleSix.Core.Mappers
     public class MapFromEntityAttribute<TSource> : Attribute
         where TSource : class, IEntity
     {
+        /// <summary>
+        /// Cấu hình auto mapper từ type chỉ định.
+        /// </summary>
+        /// <param name="ignoreProperties">Danh sách property đích sẽ không mapping.</param>
+        public MapFromEntityAttribute(params string[] ignoreProperties)
+        {
+            IgnoreProperties = ignoreProperties;
+        }
+
+        /// <summary>
+        /// Danh sách property đích sẽ không mapping.
+        /// </summary>
+        public string[]? IgnoreProperties { get; }
     }
 
     /// <summary>
@@ -21,5 +34,13 @@ namespace TripleSix.Core.Mappers
     public class MapFromEntityAttribute<TSource, TMappingAction> : MapFromEntityAttribute<TSource>
         where TSource : class, IEntity
     {
+        /// <summary>
+        /// Cấu hình auto mapper từ type chỉ định.
+        /// </summary>
+        /// <param name="ignoreProperties">Danh sách property đích sẽ không mapping.</param>
+        public MapFromEntityAttribute(params string[] ignoreProperties)
+            : base(ignoreProperties)
+        {
+        }
     }
 }
