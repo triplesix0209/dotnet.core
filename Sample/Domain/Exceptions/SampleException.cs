c1d70c7a87e759454121b9edd720b53bced33af8
Luc THQ <triplesix0209@gmail.com>
2024-10-10 12:05:46 +0700

    up

---
+++ Sample/Domain/Exceptions/SampleException.cs
@@ -0,0 +1,12 @@
+﻿namespace Sample.Domain.Exceptions
+{
+    public class SampleException : BaseException
+    {
+        public SampleException()
+            : base("Sample exception")
+        {
+        }
+
+        public override int HttpCodeStatus => 500;
+    }
+}
