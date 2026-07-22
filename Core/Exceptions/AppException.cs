87f39835419d3e5788526311cb90f34389548556
Luc THQ <triplesix0209@gmail.com>
2026-07-22 15:15:12 +0700

    up

---
+++ Core/Exceptions/AppException.cs
@@ -0,0 +1,19 @@
+﻿namespace TripleSix.Core.Exceptions
+{
+    /// <summary>
+    /// Lỗi hệ thống.
+    /// </summary>
+    public class AppException : BaseException
+    {
+        /// <summary>
+        /// Khởi tạo <see cref="AppException"/>.
+        /// </summary>
+        /// <param name="msg">Thông báo lỗi.</param>
+        /// <param name="status">Mã trạng thái HTTP.</param>
+        public AppException(string msg, int status = 500)
+            : base(msg)
+        {
+            HttpCodeStatus = status;
+        }
+    }
+}
