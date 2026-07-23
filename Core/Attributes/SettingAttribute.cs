65d8efd9e636789bcc3f5028604167ce681bc2d6
Luc THQ <triplesix0209@gmail.com>
2026-07-23 21:47:08 +0700

    up

---
+++ Core/Attributes/SettingAttribute.cs
@@ -0,0 +1,36 @@
+﻿namespace TripleSix.Core.Attributes
+{
+    /// <summary>
+    /// Thiết lập cho Setting.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All)]
+    public class SettingAttribute : Attribute
+    {
+        /// <summary>
+        /// Quy định mã code cho đối tượng.
+        /// </summary>
+        public SettingAttribute()
+        {
+        }
+
+        /// <summary>
+        /// Giá trị mặc định.
+        /// </summary>
+        public string? DefaultValue { get; set; }
+
+        /// <summary>
+        /// Mô tả.
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Công khai hay không ?.
+        /// </summary>
+        public bool IsPublic { get; set; } = false;
+
+        /// <summary>
+        /// Cho phép cập nhật hay không ?.
+        /// </summary>
+        public bool CanUpdate { get; set; } = false;
+    }
+}
