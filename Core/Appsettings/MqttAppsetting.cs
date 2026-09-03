9c7267864efec516307665bce4bf35a8d5824e34
Luc THQ <triplesix0209@gmail.com>
2026-09-03 16:28:39 +0700

    support MQTT request

---
+++ Core/Appsettings/MqttAppsetting.cs
@@ -0,0 +1,57 @@
+using Microsoft.Extensions.Configuration;
+using TripleSix.Core.Helpers;
+
+namespace TripleSix.Core.Appsettings
+{
+    /// <summary>
+    /// Cấu hình MQTT.
+    /// </summary>
+    public class MqttAppsetting : BaseAppsetting
+    {
+        /// <summary>
+        /// Khởi tạo cấu hình MQTT.
+        /// </summary>
+        /// <param name="configuration"><see cref="IConfiguration"/>.</param>
+        public MqttAppsetting(IConfiguration configuration)
+            : base(configuration, "Mqtt")
+        {
+            if (Enable && Host.IsNullOrEmpty())
+                throw new ArgumentException(nameof(Host));
+        }
+
+        /// <summary>
+        /// Bật/tắt kết nối MQTT.
+        /// </summary>
+        public bool Enable { get; set; } = false;
+
+        /// <summary>
+        /// Host kết nối MQTT broker.
+        /// </summary>
+        public string Host { get; set; } = "localhost";
+
+        /// <summary>
+        /// Port kết nối MQTT broker (mặc định 1883).
+        /// </summary>
+        public int Port { get; set; } = 1883;
+
+        /// <summary>
+        /// Username xác thực MQTT broker.
+        /// </summary>
+        public string? Username { get; set; }
+
+        /// <summary>
+        /// Password xác thực MQTT broker.
+        /// </summary>
+        public string? Password { get; set; }
+
+        /// <summary>
+        /// ClientId định danh thiết bị hoặc worker khi kết nối tới MQTT broker.
+        /// </summary>
+        public string? ClientId { get; set; }
+
+        /// <summary>
+        /// Tiền tố topic dùng chung.
+        /// </summary>
+        public string? TopicPrefix { get; set; }
+    }
+}
